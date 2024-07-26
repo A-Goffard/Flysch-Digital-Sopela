9640b00df64ffab8c392d99473209eb02425bcc6
A-Goffard <aintzagoffard@gmail.com>
2024-07-26 08:37:55 +0200

    reiniciando que se ha pedado

---
+++ src/app/pages/prueba1/prueba1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba1',
+  standalone: true,
+  imports: [],
+  templateUrl: './prueba1.component.html',
+  styleUrl: './prueba1.component.css'
+})
+export class Prueba1Component {
+
+}
---
+++ src/app/pages/prueba2/prueba2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba2',
+  standalone: true,
+  imports: [],
+  templateUrl: './prueba2.component.html',
+  styleUrl: './prueba2.component.css'
+})
+export class Prueba2Component {
+
+}
