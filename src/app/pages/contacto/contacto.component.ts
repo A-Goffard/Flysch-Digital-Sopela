b85ab595ae75596f2b89fe6149103535e5952fd6
A-Goffard <aintzagoffard@gmail.com>
2024-07-28 23:41:49 +0200

    pagina de contacto hecha

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  salida(callback: () => void) {
+    const cont = document.getElementById('cont');
 
+    if (cont) {
+      cont.classList.remove('animate__fadeIn');
+      cont.classList.add('animate__fadeOut');
+    }
+
+    setTimeout(callback, 700);
+  }
 }
+
