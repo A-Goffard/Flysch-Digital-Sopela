c903a4915e2acc4a4b8c97a92511f52f2c3d012b
A-Goffard <aintzagoffard@gmail.com>
2024-07-28 23:48:35 +0200

    pagina de contacto hecha2

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
@@ -8,6 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  constructor(private router: Router ) {}
+
+  goToHome() {
+    this.salida(() => {
+      this.router.navigate(['/home']);
+    });
+  }
+
   salida(callback: () => void) {
     const cont = document.getElementById('cont');
 
